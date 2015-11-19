1c58094d506036a73e4ccbb9574d0b9754d1d2f7
brotherirv <irvin1024@yahoo.com>
2015-11-18 20:57:16 -0800

    added intro page, links to first town level. still needs hit detection, as well as gifs and add different direction while walking

--- js/script.js
+++ js/script.js
@@ -1,4 +1,60 @@
-// (function() {
-//   alert("hello world!");
-//
-// }).call(this);
+var town;
+var player;
+var leftArrowDown = false;
+var rightArrowDown = false;
+var upArrowDown = false;
+var downArrowDown = false;
+const PLAYER_SPEED = 10;
+
+function startGame(){
+	document.getElementById('introScreen').style.display = 'none';
+}
+
+
+function initializeGame(){
+	town = document.getElementById('town');
+	town.style.left = '0px';
+	town.style.top = '0px';
+
+	
+
+	player = document.getElementById('player');
+	player.style.left = '500px';
+	player.style.top = '250px';
+
+	gameTimer = setInterval(gameloop, 50)
+}
+
+document.addEventListener('keydown', function(event){
+	if(event.keyCode==68) leftArrowDown = true;
+	if(event.keyCode==65) rightArrowDown = true;
+	if(event.keyCode==87) upArrowDown = true;
+	if(event.keyCode==83) downArrowDown = true;
+});
+
+document.addEventListener('keyup', function(event){
+	if(event.keyCode==68) leftArrowDown = false;
+	if(event.keyCode==65) rightArrowDown = false;
+	if(event.keyCode==87) upArrowDown = false;
+	if(event.keyCode==83) downArrowDown = false;
+});
+
+function gameloop(){
+	var townX = parseInt(town.style.left);
+	var townY = parseInt(town.style.top);
+	if(leftArrowDown){
+		town.style.left = townX - PLAYER_SPEED + 'px';
+	}
+	if(rightArrowDown){
+		town.style.left = townX + PLAYER_SPEED + 'px';
+	}
+	
+	if(upArrowDown){
+		town.style.top = townY + PLAYER_SPEED + 'px';
+	}
+
+	if(downArrowDown){
+		town.style.top = townY - PLAYER_SPEED + 'px';
+	}
+}
+
